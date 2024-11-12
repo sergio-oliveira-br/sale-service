daf41a185b5314f65795f046a5834a5493110293
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-12 11:48:46 +0000

    Update InventoryClientService.java
    This is a test

--- src/main/java/com/alucontrol/saleservice/service/InventoryClientService.java
+++ src/main/java/com/alucontrol/saleservice/service/InventoryClientService.java
@@ -3,6 +3,7 @@ package com.alucontrol.saleservice.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class InventoryClientService {
@@ -11,7 +12,15 @@ public class InventoryClientService {
 
     @Autowired
     public InventoryClientService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://localhost:8081").build();
+        this.webClient = webClientBuilder
+                .baseUrl("http://localhost:8081/api/v1/products")  // URL do inventory-service
+                .build();
     }
 
+    public Mono<Void> getInventoryByProductId(int productId) {
+        return webClient.get()
+                .uri("/{productId}", productId)
+                .retrieve()
+                .bodyToMono(void.class);
+    }
 }
