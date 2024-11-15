dbf671212823a5f6f175987d6d30a59bda1575c2
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-15 22:43:53 +0000

    Update InventoryClient.java
    Approach changed
    
    I getting few issues using this one, so I will start using Feign 'cause is easy and simple
    
    Act:
    -> Cleaning

--- src/main/java/com/alucontrol/saleservice/service/client/InventoryClient.java
+++ src/main/java/com/alucontrol/saleservice/service/client/InventoryClient.java
@@ -1,57 +1,9 @@
 package com.alucontrol.saleservice.service.client;
 
-import com.alucontrol.saleservice.tracking.LogUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Service
 public class InventoryClient {
 
 
-
-    private final WebClient webClient;
-
-    @Autowired
-    public InventoryClient(WebClient.Builder webClientBuilder) {
-
-        this.webClient = webClientBuilder
-            .baseUrl("http://localhost:8080/api/v1/products")  // URL do inventory-service
-            .build();
-    }
-
-    //This will ensure that the quantity requested is in stock
-    public Mono<Boolean> hasStock(Long productId, int requestedQuantity) {
-
-        LogUtil.logServiceRequest("Verificando o estoque do produto:" + productId + " quantidade:" + requestedQuantity);
-
-        return webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/check-inventory/product-id/{productId}")
-                .queryParam("requestedQuantity", requestedQuantity)
-                .build(productId))
-            .retrieve()
-            .bodyToMono(boolean.class)
-            .doOnNext(stock -> System.out.println("Resposta do InventoryService - Estoque suficiente: " + stock)
-        );
-    }
-
-    // This will reduce the stock on inventory serice
-    public Mono<Void> reduceStock(Long productId, int requestedQuantity) {
-
-        return Mono.defer(() -> {
-            LogUtil.logServiceRequest("Inicializando o processo de redução do produto '" +
-                    productId + "' quantidade '" + requestedQuantity + "'");
-
-        return webClient.put()
-            .uri(uriBuilder -> uriBuilder
-                .path("/decrease-stock/product-id/{productId}")
-                .queryParam("requestedQuantity", requestedQuantity)
-                .build(productId))
-            .retrieve()
-            .bodyToMono(void.class)
-                .onErrorResume(throwable -> Mono.empty());
-        });
-    }
 }
