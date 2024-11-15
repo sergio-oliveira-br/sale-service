6e1c685064c8ab3fada308458482a3b5e90aae5f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-15 22:34:53 +0000

    Update CreateSalesController.java
    Act:
    -> this was an attempted to make a microservices talk each other, but unfortunately is not working

--- src/main/java/com/alucontrol/saleservice/controller/CreateSalesController.java
+++ src/main/java/com/alucontrol/saleservice/controller/CreateSalesController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.View;
 import reactor.core.publisher.Mono;
 
 
@@ -17,11 +18,13 @@ public class CreateSalesController {
 
     private final CreateSalesService createSalesService;
     private final InventoryClient inventoryClient;
+    private final View error;
 
     @Autowired
-    public CreateSalesController(CreateSalesService createSalesService, InventoryClient inventoryClient) {
+    public CreateSalesController(CreateSalesService createSalesService, InventoryClient inventoryClient, View error) {
         this.createSalesService = createSalesService;
         this.inventoryClient = inventoryClient;
+        this.error = error;
     }
 
     @PostMapping("/product/{productId}")
@@ -34,8 +37,19 @@ public class CreateSalesController {
         return inventoryClient.hasStock(productId, requestedQuantity)
             .flatMap(hasStock -> {
                 if (hasStock) {
-                    createSalesService.saveSale(sale);
-                    return Mono.just(ResponseEntity.status(HttpStatus.CREATED).body("Venda criada com sucesso."));
+
+                    return inventoryClient.reduceStock(productId, requestedQuantity).onErrorResume(error -> {
+                        LogUtil.error("Error reducing stock: " + error.getMessage());
+                        // Optionally, log the error or take alternative actions
+                        return Mono.empty(); // Continue with sale creation despite stock reduction error (may need further handling)
+                    })
+                    .then(Mono.defer(() -> { // Defer execution of saveSale until stock reduction completes
+                        createSalesService.saveSale(sale);
+                        return Mono.just(ResponseEntity.status(HttpStatus.CREATED).body("Venda criada com sucesso."));
+                    }));
+
+//                    createSalesService.saveSale(sale);
+//                    return Mono.just(ResponseEntity.status(HttpStatus.CREATED).body("Venda criada com sucesso."));
                 }
                 else {
                     return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não foi possivel criar a venda. Estoque insuficiente."));
