cdd1931c87bb9831fc2aae43a4f50d1db00888eb
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-17 18:20:17 +0000

    Update SaleServiceApplication.java
    Act:
    -> Feign annotation added

--- src/main/java/com/alucontrol/saleservice/SaleServiceApplication.java
+++ src/main/java/com/alucontrol/saleservice/SaleServiceApplication.java
@@ -2,8 +2,10 @@ package com.alucontrol.saleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SaleServiceApplication {
 
     public static void main(String[] args) {
