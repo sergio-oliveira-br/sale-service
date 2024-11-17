59af0ea34f7109330cc97d5dc29374595da0f6b1
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-17 19:10:38 +0000

    Update InventoryClient.java
    Act:
    ->  this is the client that will make the HTTP request for the inventory microservice (created)

--- src/main/java/com/alucontrol/saleservice/client/InventoryClient.java
+++ src/main/java/com/alucontrol/saleservice/client/InventoryClient.java
@@ -3,6 +3,7 @@ package com.alucontrol.saleservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "inventory-service", url = "http://localhost:8080/api/v1/products")
@@ -11,4 +12,9 @@ public interface InventoryClient {
     @GetMapping("/check-inventory/product-id/{productId}")
     Boolean checkInventory(@PathVariable("productId") Long productId,
                            @RequestParam("requestedQuantity") int requestedQuantity);
+
+
+    @PutMapping("/decrease-stock/product-id/{productId}")
+    void decreaseStock(@PathVariable("productId") Long productId,
+                       @RequestParam int requestedQuantity);
 }
