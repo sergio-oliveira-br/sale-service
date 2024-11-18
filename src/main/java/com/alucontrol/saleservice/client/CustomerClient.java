3fb7cdf344d7d69f4ac157ec807421363c10313b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-18 21:13:08 +0000

    Create CustomerClient.java
    Act:
    -> this is the feign client, that will get the info from customer-service
    
    -> this method request the name of the customer

---
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -0,0 +1,12 @@
+package com.alucontrol.saleservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "customer-service", url = "http://localhost:8082/api/v1/customers")
+public interface CustomerClient {
+
+    @GetMapping("/{id}")
+    String requestCustomerNameById(@PathVariable("id") Long id);
+}
