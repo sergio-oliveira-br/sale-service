bea3d55f32f7c7f443308069aa5462fdbaa65727
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-18 22:12:31 +0000

    Update GlobalExceptionHandler.java
    Act:
    -> message updated

--- src/main/java/com/alucontrol/saleservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/alucontrol/saleservice/exceptions/GlobalExceptionHandler.java
@@ -20,9 +20,9 @@ public class GlobalExceptionHandler {
 
         ProblemDetails problemDetails = new ProblemDetails(
                 HttpStatus.NOT_FOUND.toString(),
-                "Resource Not Found",
+                "Sale Not Found",
                 ex.getMessage(),
-                "O recurso solicitado não foi encontrado",
+                "A venda solicitada não foi encontrado",
                 UUID.randomUUID()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetails);
