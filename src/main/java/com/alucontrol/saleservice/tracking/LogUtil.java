613524f551de57d9bb14c1ce1b951aad0baba30c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-18 17:47:39 +0000

    Update LogUtil.java
    Act:
    -> logs returned
    
    -> traceID removed

--- src/main/java/com/alucontrol/saleservice/tracking/LogUtil.java
+++ src/main/java/com/alucontrol/saleservice/tracking/LogUtil.java
@@ -10,38 +10,29 @@ public class LogUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(LogUtil.class);
 
-
-    // Método para logar o traceId
-    public static void logTraceId() {
-        String traceId = MDC.get("X-B3-TraceId");
-        logger.info("Trace ID: {}", traceId);
+    public static void info(String message) {
+        logger.info("[{}] {}", MDC.get("requestId"), message);
     }
 
+    public static void info(String message, Throwable throwable) {
+        logger.info("[{}] {}", MDC.get("requestId"), message, throwable);
+    }
 
-//
-//    public static void info(String message) {
-//        logger.info("[{}] {}", MDC.get("requestId"), message);
-//    }
-//
-//    public static void info(String message, Throwable throwable) {
-//        logger.info("[{}] {}", MDC.get("requestId"), message, throwable);
-//    }
-//
-//    public static void error(String message) {
-//        logger.error("[{}] {}", MDC.get("requestId"), message);
-//    }
-//
-//    public static void error(String message, Throwable throwable) {
-//        logger.error("[{}] {}", MDC.get("requestId"), message, throwable);
-//    }
-//
-//    //Specific logs for database operations
-//    public static void logDatabaseOperation(String message) {
-//        logger.info("[{}] [Database] {}", MDC.get("requestId"), message);
-//    }
-//
-//    //Specific logs for requests to other microservices
-//    public static void logServiceRequest(String message) {
-//        logger.info("[{}] [ServiceRequest] {}", MDC.get("requestId"), message);
-//    }
+    public static void error(String message) {
+        logger.error("[{}] {}", MDC.get("requestId"), message);
+    }
+
+    public static void error(String message, Throwable throwable) {
+        logger.error("[{}] {}", MDC.get("requestId"), message, throwable);
+    }
+
+    //Specific logs for database operations
+    public static void logDatabaseOperation(String message) {
+        logger.info("[{}] [Database] {}", MDC.get("requestId"), message);
+    }
+
+    //Specific logs for requests to other microservices
+    public static void logServiceRequest(String message) {
+        logger.info("[{}] [ServiceRequest] {}", MDC.get("requestId"), message);
+    }
 }
