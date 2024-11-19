47685e8f1d6fcda62c68ac7298b64af9a9eace2d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-19 21:55:52 +0000

    Update CustomerClient.java
    new method added

--- src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -1,12 +1,20 @@
 package com.alucontrol.saleservice.client;
 
+import com.alucontrol.saleservice.model.CustomerDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "customer-service")
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
     String getCustomerNameById(@PathVariable Long id);
+
+
+    @GetMapping("/api")
+    List<CustomerDTO> getCustomerName();
+
 }
