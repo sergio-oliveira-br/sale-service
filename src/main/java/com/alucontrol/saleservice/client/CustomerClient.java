2fd8a33baedb7a85482b788df83b9d2777207f35
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-19 18:12:18 +0000

    Update CustomerClient.java
    act:
    -> url removed 'cause the eureka now will fetch the service
    
    -> part of URI added in the mapping annotation

--- src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", url = "http://localhost:8082/api/v1/customers")
+@FeignClient(name = "customer-service")
 public interface CustomerClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/customers/{id}")
     String getCustomerNameById(@PathVariable("id") Long id);
 }
