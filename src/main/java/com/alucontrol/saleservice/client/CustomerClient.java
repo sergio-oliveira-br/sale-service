fecf1477a060d340f2d7633fe5a8dd1ba3aec7ce
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-19 21:52:09 +0000

    Update CustomerClient.java
    act:
    -> unnecessary info removed

--- src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
-    String getCustomerNameById(@PathVariable("id") Long id);
+    String getCustomerNameById(@PathVariable Long id);
 }
