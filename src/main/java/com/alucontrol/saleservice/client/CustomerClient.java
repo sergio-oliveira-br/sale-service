1f729b1058eeb95c31abfc01ae9c08225e5fc2bc
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-20 12:41:42 +0000

    Update CustomerClient.java
    setting better names

--- src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -1,6 +1,7 @@
 package com.alucontrol.saleservice.client;
 
 import com.alucontrol.saleservice.model.CustomerDTO;
+import com.alucontrol.saleservice.tracking.LogUtil;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,10 @@ import java.util.List;
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
-    String getCustomerNameById(@PathVariable Long id);
+    String findCustomerNameById(@PathVariable Long id);
 
 
-    @GetMapping("/api")
-    List<CustomerDTO> getCustomerName();
+    @GetMapping("/api/v1/customers")
+    List<CustomerDTO> findCustomerName();
 
 }
