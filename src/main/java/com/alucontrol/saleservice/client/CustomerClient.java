ccb266c61039acf79cb6a9cb28ca823e1dd99761
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-27 13:40:37 +0000

    Update CustomerClient.java
    issue:
    # I cannot access the Eureka services
    
    act:
    -> url added to eliminate the Eureka route

--- src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "customer-service")
+@FeignClient(name = "customer-service", url = "http://localhost:8082")
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
