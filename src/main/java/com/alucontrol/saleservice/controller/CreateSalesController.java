ad5494caeef370c29b272e1d168aa7f5a9e8fddd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-27 13:42:07 +0000

    Update CreateSalesController.java
    act:
    -> Request for finance service: send the amout to finance service added

--- src/main/java/com/alucontrol/saleservice/controller/CreateSalesController.java
+++ src/main/java/com/alucontrol/saleservice/controller/CreateSalesController.java
@@ -1,19 +1,18 @@
 package com.alucontrol.saleservice.controller;
 
-import com.alucontrol.saleservice.client.CustomerClient;
+import com.alucontrol.saleservice.client.FinanceClient;
 import com.alucontrol.saleservice.client.InventoryClient;
 import com.alucontrol.saleservice.entity.Sale;
 import com.alucontrol.saleservice.exceptions.InsufficientStockException;
+import com.alucontrol.saleservice.model.FinanceDTO;
 import com.alucontrol.saleservice.service.business.CreateSalesService;
-//import com.alucontrol.saleservice.tracking.LogUtil;
 import com.alucontrol.saleservice.tracking.LogUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 //import org.springframework.web.server.ResponseStatusException;
 
 
@@ -23,18 +22,21 @@ public class CreateSalesController {
 
     private final CreateSalesService createSalesService;
     private final InventoryClient inventoryClient;
+    private final FinanceClient financeClient;
 
     @Autowired
     public CreateSalesController(CreateSalesService createSalesService,
-                                 InventoryClient inventoryClient) {
+                                 InventoryClient inventoryClient, FinanceClient financeClient) {
 
         this.createSalesService = createSalesService;
         this.inventoryClient = inventoryClient;
+        this.financeClient = financeClient;
     }
 
     @PostMapping("/product/{productId}")
     public ResponseEntity<Sale> createSale(@PathVariable("productId") Long productId,
                                                    @RequestParam("requestedQuantity") int requestedQuantity,
+                                                   @RequestParam BigDecimal amount,
                                                    @RequestBody Sale sale) {
 
         // Request for inventory service: check stock availability
@@ -47,6 +49,10 @@ public class CreateSalesController {
 
             // Request for inventory service: increase sold qty field
             inventoryClient.increaseSold(productId, requestedQuantity);
+
+            // Request for finance service: send the amout to finance service
+            FinanceDTO financeDTO = new FinanceDTO(amount); // Criação do DTO
+            financeClient.addOfIncome(financeDTO); // Enviar o DTO
 
             // Internal service: save the details of the sale in the local database
             createSalesService.saveSale(sale);
