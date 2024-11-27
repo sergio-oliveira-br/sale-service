fc064119ccfd6d51ce30d54915db2d084b4ccff0
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-27 13:41:38 +0000

    Create FinanceClient.java
    act:
    -> this is responsible to access the finance microservice, note that I cannot access the Eureka services, so the url was added to eliminate the Eureka route

---
+++ src/main/java/com/alucontrol/saleservice/client/FinanceClient.java
@@ -0,0 +1,17 @@
+package com.alucontrol.saleservice.client;
+
+import com.alucontrol.saleservice.model.FinanceDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "finance-service", url = "http://localhost:8083")
+public interface FinanceClient {
+
+    @PostMapping("/api/v1/finance/income")
+    void addOfIncome (@RequestBody FinanceDTO financeDTO);
+}
