690743f4361fa847db3916bebd86ae15ea2c9d32
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-02 10:53:40 +0000

    Update CustomerService.java
    act:
    -> fallback created to test and learn

--- src/main/java/com/alucontrol/saleservice/service/external/CustomerService.java
+++ src/main/java/com/alucontrol/saleservice/service/external/CustomerService.java
@@ -3,6 +3,7 @@ package com.alucontrol.saleservice.service.external;
 import com.alucontrol.saleservice.client.CustomerClient;
 import com.alucontrol.saleservice.model.CustomerDTO;
 import com.alucontrol.saleservice.tracking.LogUtil;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +20,27 @@ public class CustomerService {
     }
 
     // This service is responsible to get a unique customer name by id
+    @CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetCustomerNameById")
     public String getCustomerNameById(Long id) {
         LogUtil.info("Calling Customer Cliente to get customer name by Id: " + id);
         return customerClient.findCustomerNameById(id);
     }
 
+    // Fallback method para o getCustomerNameById
+    public String fallbackGetCustomerNameById(Long id, Throwable t) {
+        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
+        return "Default Customer Name"; // Retorna um valor default em caso de falha
+    }
+
+    @CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetAllCustomersName")
     public List<CustomerDTO> getAllCustomersName() {
 
         return customerClient.findCustomerName();
+    }
+
+    // Fallback method para o getAllCustomersName
+    public List<CustomerDTO> fallbackGetAllCustomersName(Throwable t) {
+        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
+        return List.of(); // Retorna uma lista vazia em caso de falha
     }
 }
