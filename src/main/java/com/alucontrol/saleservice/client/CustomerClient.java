141868c71aa907774296ead0e2238a3a3b20c245
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-05 14:22:11 +0000

    Update CustomerClient.java
    act:
    -> this is attempt to make my microservice work using Eureka Services
    
    unfortunately is not working

--- src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
+++ src/main/java/com/alucontrol/saleservice/client/CustomerClient.java
@@ -3,19 +3,21 @@ package com.alucontrol.saleservice.client;
 import com.alucontrol.saleservice.model.CustomerDTO;
 import com.alucontrol.saleservice.tracking.LogUtil;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "customer-service")
+@FeignClient(value = "customer-service", url = "http://localhost:8082")
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
     String findCustomerNameById(@PathVariable Long id);
 
 
-    @GetMapping("/api/v1/customers")
+    @GetMapping()
     List<CustomerDTO> findCustomerName();
 
-}
+    }
+
