f53f19e2e9253390aa1b3e7550a132dc656f413b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-05 14:21:27 +0000

    Update CustomerService.java
    act:
    -> this is attempt to make my microservice work using Eureka Services
    
    unfortunately is not working

--- src/main/java/com/alucontrol/saleservice/service/external/CustomerService.java
+++ src/main/java/com/alucontrol/saleservice/service/external/CustomerService.java
@@ -20,27 +20,27 @@ public class CustomerService {
     }
 
     // This service is responsible to get a unique customer name by id
-    @CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetCustomerNameById")
+    //@CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetCustomerNameById")
     public String getCustomerNameById(Long id) {
         LogUtil.info("Calling Customer Cliente to get customer name by Id: " + id);
         return customerClient.findCustomerNameById(id);
     }
 
-    // Fallback method para o getCustomerNameById
-    public String fallbackGetCustomerNameById(Long id, Throwable t) {
-        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
-        return "Default Customer Name"; // Retorna um valor default em caso de falha
-    }
+//    // Fallback method para o getCustomerNameById
+//    public String fallbackGetCustomerNameById(Long id, Throwable t) {
+//        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
+//        return "Default Customer Name"; // Retorna um valor default em caso de falha
+//    }
 
-    @CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetAllCustomersName")
+    //@CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetAllCustomersName")
     public List<CustomerDTO> getAllCustomersName() {
-
         return customerClient.findCustomerName();
     }
 
-    // Fallback method para o getAllCustomersName
-    public List<CustomerDTO> fallbackGetAllCustomersName(Throwable t) {
-        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
-        return List.of(); // Retorna uma lista vazia em caso de falha
-    }
+
+//    // Fallback method para o getAllCustomersName
+//    public List<CustomerDTO> fallbackGetAllCustomersName(Throwable t) {
+//        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
+//        return List.of(); // Retorna uma lista vazia em caso de falha
+//    }
 }
