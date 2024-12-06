b6d0de782d10d856f78d497f9c62641236d1ecca
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-06 20:02:49 +0000

    Update InventoryClient.java

--- src/main/java/com/alucontrol/saleservice/client/InventoryClient.java
+++ src/main/java/com/alucontrol/saleservice/client/InventoryClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory-service")
+@FeignClient(name = "inventory-service", url = "http://localhost:8081")
 public interface InventoryClient {
 
     @GetMapping("/api/v1/products/check-inventory/product-id/{productId}")
