88b94c258b0fc02e5bb72af978be25626a80700d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-06 20:02:47 +0000

    Update FinanceClient.java
    act:
    -> url added to use Feign services

--- src/main/java/com/alucontrol/saleservice/client/FinanceClient.java
+++ src/main/java/com/alucontrol/saleservice/client/FinanceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 
-@FeignClient("finance-service")
+@FeignClient(value = "finance-service", url = "http://localhost:8083")
 public interface FinanceClient {
 
     @PostMapping("/api/v1/finance/income")
