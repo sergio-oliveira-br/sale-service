a22623bf02b89aa4658eddb93a87b366c958e1b3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-09 13:25:26 +0000

    Update Sale.java
    act:
    -> validation added

--- src/main/java/com/alucontrol/saleservice/entity/Sale.java
+++ src/main/java/com/alucontrol/saleservice/entity/Sale.java
@@ -22,9 +22,15 @@ public class Sale {
     @NotNull(message = "O valor 'amount' da venda deve ser informado")
     private BigDecimal amount;
 
+    @NotNull(message = "A quantidade 'quantity_sold' vendida deve ser informada")
     private int quantitySold;
+
+    @NotNull(message = "O id do cliente deve ser mencionado")
     private Long customerId;
+
     private String customerName;
+
+    @NotNull(message = "O id do produto é um campo obrigatorio")
     private Long productId;
 
     @CreationTimestamp
