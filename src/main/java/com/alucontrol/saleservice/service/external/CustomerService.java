7a4ede0f4515765f6b2eea20cd4cbf78c78a5b5f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-12-10 11:46:36 +0000

    Update CustomerService.java
    circuit breaker resilience4j added
    
    act:
    -> fallback added

--- src/main/java/com/alucontrol/saleservice/service/external/CustomerService.java
+++ src/main/java/com/alucontrol/saleservice/service/external/CustomerService.java
@@ -21,28 +21,31 @@ public class CustomerService {
     }
 
     // This service is responsible to get a unique customer name by id
-    //@CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetCustomerNameById")
+    @CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetCustomerNameById")
     public String getCustomerNameById(Long id) {
         return customerClient.findCustomerNameById(id);
     }
 
-//    // Fallback method para o getCustomerNameById
-//    public String fallbackGetCustomerNameById(Long id, Throwable t) {
-//        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
-//        return "Default Customer Name"; // Retorna um valor default em caso de falha
-//    }
+    // Fallback method para o getCustomerNameById
+    public String fallbackGetCustomerNameById(Long id, Throwable t) {
+        LogUtil.error("Ocorreu um erro ao chamar o CustomerClient: " + t.getMessage());
+        return "Retornando uma mensagem genérica informando a existencia da falha no getCustomerNameById()";
+    }
 
-    //@CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetAllCustomersName")
+
+
+    @CircuitBreaker(name = "customerServiceCircuitBreaker", fallbackMethod = "fallbackGetAllCustomersName")
     public List<CustomerDTO> getAllCustomersName() {
         return customerClient.findCustomerName();
     }
 
+    // Fallback method para o getAllCustomersName
+    public String fallbackGetAllCustomersName(Throwable t) {
+        LogUtil.error("Ocorreu um erro ao chamar o CustomerClient: " + t.getMessage());
+        return "Retornando uma mensagem genérica informando a existencia da falha no getAllCustomersName() ";
+    }
 
-//    // Fallback method para o getAllCustomersName
-//    public List<CustomerDTO> fallbackGetAllCustomersName(Throwable t) {
-//        LogUtil.error("Error occurred while calling CustomerClient: " + t.getMessage());
-//        return List.of(); // Retorna uma lista vazia em caso de falha
-//    }
+
 
     public CustomerDTO updateCustomerTotalSpent(Long id, BigDecimal amountSpent) {
         return customerClient.updateCustomer(id, amountSpent);
